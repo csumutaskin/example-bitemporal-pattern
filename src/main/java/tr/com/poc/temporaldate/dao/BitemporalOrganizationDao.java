59741ce91c97536dd5c2a8eeb4f44f968e84551a
csumutaskin <umutaskin@gmail.com>
2019-03-27 23:45:43 +0300

    minor bug fix on repository layer

--- src/main/java/tr/com/poc/temporaldate/dao/BitemporalOrganizationDao.java
+++ src/main/java/tr/com/poc/temporaldate/dao/BitemporalOrganizationDao.java
@@ -1,7 +1,10 @@
 package tr.com.poc.temporaldate.dao;
 
+import org.springframework.stereotype.Repository;
+
 import tr.com.poc.temporaldate.core.dao.impl.BaseBiTemporalDaoImpl;
 import tr.com.poc.temporaldate.model.BitemporalOrganization;
 
+@Repository
 public class BitemporalOrganizationDao extends BaseBiTemporalDaoImpl<BitemporalOrganization>
 {}
