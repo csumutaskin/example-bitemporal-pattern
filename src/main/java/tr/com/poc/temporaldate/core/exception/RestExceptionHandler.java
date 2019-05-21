70c4b3acc0b176e9d505bdaadd982b8111524126
Kazım Umut Aşkın <umut.askin@epias.com.tr>
2019-05-21 16:51:41 +0300

    to be continued...

--- src/main/java/tr/com/poc/temporaldate/core/exception/RestExceptionHandler.java
+++ src/main/java/tr/com/poc/temporaldate/core/exception/RestExceptionHandler.java
@@ -1,17 +1,20 @@
 package tr.com.poc.temporaldate.core.exception;
 
+import static tr.com.poc.temporaldate.common.Constants.MDC_HOST_ADDRESS;
+import static tr.com.poc.temporaldate.common.Constants.MDC_TRANSACTION_NO;
+import static tr.com.poc.temporaldate.common.Constants.MDC_USERNAME;
 import static tr.com.poc.temporaldate.common.ExceptionConstants.UNEXPECTED_EXCEPTION;
 
+import java.util.Deque;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -33,27 +36,37 @@ public class RestExceptionHandler
 		this.messageSource = messageSource;
 	}
 
+	@SuppressWarnings("unchecked")
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<RestResponse<BaseExceptionDTO>> handleBusinessException(RestException ex, Locale locale) 
+	public ResponseEntity<RestResponse<BaseExceptionDTO>> handleBusinessException(BusinessException bexc, Locale locale) 
 	{
-		String errorMessage = messageSource.getMessage(ex.getMessage(), ex.getArgs(), locale);
-		return new ResponseEntity<>(new RestResponse(errorMessage), HttpStatus.BAD_REQUEST);
+		String errorMessage = messageSource.getMessage(bexc.getExceptionCode(), bexc.getExceptionMessageParameters(), locale);
+		return new ResponseEntity<>(new RestResponse(HttpStatus.BAD_REQUEST.toString(), ThreadContext.get(MDC_TRANSACTION_NO), ThreadContext.get(MDC_HOST_ADDRESS), ThreadContext.get(MDC_USERNAME),null, null), HttpStatus.BAD_REQUEST);
 	}
 
 	@ExceptionHandler(ApplicationException.class)
-	public ResponseEntity<RestMessage> handleApplicationException(MethodArgumentNotValidException ex, Locale locale) 
+	public ResponseEntity<RestResponse<BaseExceptionDTO>> handleApplicationException(ApplicationException aexc, Locale locale) 
 	{
-		BindingResult result = ex.getBindingResult();
-		List<String> errorMessages = result.getAllErrors().stream()
-				.map(objectError -> messageSource.getMessage(objectError, locale)).collect(Collectors.toList());
-		return new ResponseEntity<>(new RestMessage(errorMessages), HttpStatus.BAD_REQUEST);
+		String errorMessage = messageSource.getMessage(aexc.getExceptionCode(), aexc.getExceptionMessageParameters(), locale);
+	//	return new ResponseEntity<>(new RestResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return null;
+	}
+	
+	@ExceptionHandler(BusinessValidationException.class)
+	public ResponseEntity<RestResponse<BaseExceptionDTO>> handleApplicationException(BusinessValidationException bvexc, Locale locale) 
+	{
+		Deque<BusinessValidationExceptionItem> businessValidationExceptionItemList = bvexc.getBusinessValidationExceptionItemList();
+	//	List<String> errorMessages = businessValidationExceptionItemList.stream().map(objectError -> messageSource.getMessage(objectError, locale)).collect(Collectors.toList());
+	//	return new ResponseEntity<>(new RestResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return null;
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<RestMessage> handleExceptions(Exception ex, Locale locale) 
+	public ResponseEntity<RestResponse<BaseExceptionDTO>> handleExceptions(Exception exc, Locale locale) 
 	{
-		String errorMessage = messageSource.getMessage(UNEXPECTED_ERROR, null, locale);
-		ex.printStackTrace();
-		return new ResponseEntity<>(new RestMessage(errorMessage), HttpStatus.INTERNAL_SERVER_ERROR);
+		String errorMessage = messageSource.getMessage(UNEXPECTED_EXCEPTION, null, locale);
+	//	ex.printStackTrace();
+	//	return new ResponseEntity<>(new RestResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+		return null;
 	}
 }
--- src/main/java/tr/com/poc/temporaldate/core/util/RestResponse.java
+++ src/main/java/tr/com/poc/temporaldate/core/util/RestResponse.java
@@ -7,6 +7,7 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -15,6 +16,7 @@ import tr.com.poc.temporaldate.core.model.BaseDTO;
 @SuppressWarnings("serial")
 @XmlRootElement(name = "response")
 @XmlAccessorType(XmlAccessType.FIELD)
+@AllArgsConstructor
 @Getter
 @Setter
 @ToString
