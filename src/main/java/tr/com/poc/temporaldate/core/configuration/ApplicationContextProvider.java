9564beaeda13a6b807d337cae0009f15992bcd64
Aziz Keskin <aziz.keskin@epias.com.tr>
2019-07-03 10:52:14 +0300

    add custom valid annotations

--- src/main/java/tr/com/poc/temporaldate/core/configuration/ApplicationContextProvider.java
+++ src/main/java/tr/com/poc/temporaldate/core/configuration/ApplicationContextProvider.java
@@ -4,6 +4,12 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import tr.com.poc.temporaldate.common.ExceptionConstants;
+import tr.com.poc.temporaldate.core.exception.ApplicationException;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class ApplicationContextProvider implements ApplicationContextAware
 {
 
@@ -22,6 +28,14 @@ public class ApplicationContextProvider implements ApplicationContextAware
 
 	public static Object getBeanFromApplicationContext(Class<?> bean)
 	{
-		return ctx.getBean(bean);
+		try
+		{
+			return ctx.getBean(bean);
+		}
+		catch (BeansException ex)
+		{
+			log.error("Class : {} , BeansException : {}", bean.getSimpleName(), ex);
+			throw new ApplicationException(ExceptionConstants.UNEXPECTED_OBJECT_CREATION_EXCEPTION_THROUGH_REFLECTION, ex);
+		}
 	}
 }
